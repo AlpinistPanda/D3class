5f07dfb8ac157c89f08f9556b662dbd863b18ce1
Ozgun Balaban <ozgunbalaban@yahoo.com>
2019-12-16 01:02:28 +0300

    simple-barchart

---
+++ shapes.js
@@ -0,0 +1,12 @@
+var dataArray = [5, 11, 18];
+
+var svg = d3.select("body").append("svg").attr("height", "50%").attr("width", "50%");
+
+svg.selectAll("rect")
+   .data(dataArray)
+   .enter().append("rect")
+            .attr("height", function(d, i) {return 15 * d})
+            .attr("width", "50")
+            .attr("fill", "purple")
+            .attr("x", function(d,i) {return 60*i;})
+            .attr("y", function(d, i) {return 300-(d*15);});
