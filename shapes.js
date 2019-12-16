bbfef22012dd8a95e503c3345f73c6345834be41
Ozgun Balaban <ozgunbalaban@yahoo.com>
2019-12-16 09:39:27 +0300

    path

--- shapes.js
+++ shapes.js
@@ -1,46 +1,14 @@
-var dataArray = [5, 11, 18];
+var dataArray = [{x:5,y:5},{x:10,y:15},{x:20,y:7},{x:30,y:18},{x:40,y:10}];
 
 var svg = d3.select("body").append("svg").attr("height", "100%").attr("width", "100%");
 
-svg.selectAll("rect")
-   .data(dataArray)
-   .enter().append("rect")
-            .attr("height", function(d, i) {return 15 * d})
-            .attr("width", "50")
-            .attr("fill", "purple")
-            .attr("x", function(d,i) {return 60*i;})
-            .attr("y", function(d, i) {return 300-(d*15);});
+var line = d3.line()
+             .x(function(d, i) { return d.x*6})
+             .y(function(d, i) { return d.y*4;})
+             .curve(d3.curveStep);
+             // http://bl.ocks.org/d3indepth/b6d4845973089bc1012dec1674d3aff8
 
-var newX = 300;
-svg.selectAll("circle")
-   .data(dataArray)
-   .enter().append("circle")
-   .attr("cx", function(d,i) {newX += (d*3)+(i*20); return newX;})
-   .attr("cy", "100")
-   .attr("r", function(d) {return d*3;});
-
-newX = 600;
-svg.selectAll("ellipse")
-   .data(dataArray)
-   .enter().append("ellipse")
-   .attr("cx", function(d,i) {newX += (d*3)+(i*20); return newX;})
-   .attr("cy", "100")
-   .attr("rx", function(d) {return d*3;})
-   .attr("ry", "30");
-
-newX = 900;
-svg.selectAll("line")
-   .data(dataArray)
-   .enter().append("line")
-   .attr("x1", newX)
-   .attr("stroke", "red")
-   .attr("stroke-width", "2")
-   .attr("y1", function(d, i) {return 80 + (i*20);})
-   .attr("x2", function(d) {return newX+ (d*15);})
-   .attr("y2", function(d, i) {return 80 + (i*20);});
-
-
-svg.append("text")
-   .attr("x", newX)
-   .attr("y", 150)
-   .text("Hello World!");
+svg.append("path")
+   .attr("fill", "none")
+   .attr("stroke", "blue")
+   .attr("d", line(dataArray));
